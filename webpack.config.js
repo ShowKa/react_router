5be53a43f72f493cbd44a8e1eace9e1bb4f5bc49
John Doe <johndoe@example.com>
2020-08-18 11:39:17 +0900

    setup react router

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+let debug = process.env.NODE_ENV !== "production";
+let webpack = require('webpack');
+let path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  entry: "./js/client.js",
+  devtool: debug ? "inline-sourcemap" : false,
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }]
+    }]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js",
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
